70ab0d8a3f516d8d86612566fb9f1ba9284720dc
annabentzen <126067360+annabentzen@users.noreply.github.com>
2025-11-12 13:49:15 +0100

    fix: created migrations after model change in character.cs

--- server/Migrations/20251112124025_AddValidationToCharacter.Designer.cs
+++ server/Migrations/20251112124025_AddValidationToCharacter.Designer.cs
@@ -2,6 +2,7 @@
 using DragonGame.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DragonGame.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251112124025_AddValidationToCharacter")]
+    partial class AddValidationToCharacter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
