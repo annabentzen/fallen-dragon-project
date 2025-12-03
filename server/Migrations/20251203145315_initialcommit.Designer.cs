b6533868ad3dcbaf80dbacb56b80460cba956eeb
selmadyrlie <sejoh0292@oslomet.no>
2025-12-03 15:55:44 +0100

    Merge styling-home-page into styling-story-ending

--- server/Migrations/20251203145315_initialcommit.Designer.cs
+++ server/Migrations/20251203145315_initialcommit.Designer.cs
@@ -11,12 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DragonGame.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-<<<<<<<< HEAD:server/Migrations/20251203131957_InitialCreate.Designer.cs
-    [Migration("20251203131957_InitialCreate")]
-========
-    [Migration("20251203105044_InitialCreate")]
->>>>>>>> origin/styling-home-page:server/Migrations/20251203105044_InitialCreate.Designer.cs
-    partial class InitialCreate
+    [Migration("20251203145315_initialcommit")]
+    partial class initialcommit
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
